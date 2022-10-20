cc7e146cf134bcf34b4a8fae455e2b5845dca50e
asadbek-ip <dasturchi6678@gmail.com>
2022-10-20 10:40:14 +0500

    finish

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,118 +13,124 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            height: 300,
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage("assets/images/nemnig.jpg"))),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomLeft,
-                  colors: [
-                    Colors.black.withOpacity(.5),
-                    Colors.black.withOpacity(.4)
-                  ],
-                ),
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      "What kind of hotel you need?",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 30),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: 750,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 300,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/images/nemnig.jpg"))),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      colors: [
+                        Colors.black.withOpacity(.5),
+                        Colors.black.withOpacity(.4)
+                      ],
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.search,
+                  ),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "What kind of hotel you need?",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 30),
                         ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: "Search for hotels...",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(40),
-                            gapPadding: 0,
-                            borderSide: BorderSide(color: Colors.white)),
-                      ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.search,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: "Search for hotels...",
+                            hintStyle: TextStyle(color: Colors.grey),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                gapPadding: 0,
+                                borderSide: BorderSide(color: Colors.white)),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Container(
-            padding: EdgeInsets.all(12),
-            child: Text(
-              "Best Hotels",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 17),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                  _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                 _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-
-                ],
+              SizedBox(
+                height: 25,
               ),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            padding: EdgeInsets.all(8),
-            child: Text(
-              "Luxury Hotels",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 17),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                   _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                  _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                 _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                ],
+              Container(
+                padding: EdgeInsets.all(12),
+                child: Text(
+                  "Best Hotels",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 17),
+                ),
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+                      _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+                     _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+      
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  "Luxury Hotels",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 17),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                       _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+                      _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+                     _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+                    ],
+                  ),
+                ),
+              ),
+            
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
