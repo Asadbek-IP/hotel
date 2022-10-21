dc12b243e8ba5bdfcce7a70b8053337654862a68
asadbek-ip <dasturchi6678@gmail.com>
2022-10-21 16:14:48 +0500

    finish

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,124 +11,151 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: 750,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                height: 300,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/images/nemnig.jpg"))),
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomLeft,
-                      colors: [
-                        Colors.black.withOpacity(.5),
-                        Colors.black.withOpacity(.4)
-                      ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 280,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/nemnig.jpg"))),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  colors: [
+                    Colors.black.withOpacity(.5),
+                    Colors.black.withOpacity(.4)
+                  ],
+                ),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Best Hotels Ever",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 30),
                     ),
-                  ),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "What kind of hotel you need?",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 30),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.search,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            hintText: "Search for hotels...",
-                            hintStyle: TextStyle(color: Colors.grey),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(40),
-                                gapPadding: 0,
-                                borderSide: BorderSide(color: Colors.white)),
-                          ),
-                        ),
-                      ],
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
+                    TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.search,
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "Search for hotels...",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            gapPadding: 0,
+                            borderSide: const BorderSide(color: Colors.white)),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(
-                height: 25,
-              ),
-              Container(
-                padding: EdgeInsets.all(12),
-                child: Text(
-                  "Best Hotels",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 17),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                      _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                     _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-      
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                padding: EdgeInsets.all(8),
-                child: Text(
-                  "Luxury Hotels",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 17),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                       _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                      _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                     _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
-                    ],
-                  ),
-                ),
-              ),
-            
-            ],
+            ),
           ),
-        ),
+          const SizedBox(
+            height:10,
+          ),
+     Expanded(
+       child: SingleChildScrollView(
+         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+                Container(
+                padding: const EdgeInsets.all(12),
+                child: const Text(
+              "Best Hotels",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20),
+                ),
+              ),
+              Container(
+                height: 200,
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+                _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+               _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+               
+              ],
+                ),
+              ),
+              const SizedBox(
+                height:5,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text(
+              "  Luxury Hotels",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20),
+                ),
+              ),
+             Container(
+            height: 200,
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                 _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+                _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+               _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+              ],
+                ),
+              ),
+              const SizedBox(
+                height:5,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text(
+              "  Resort Hotels",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20),
+                ),
+              ),
+             Container(
+              height: 200,
+               padding: const EdgeInsets.all(8.0),
+               child: ListView(
+             scrollDirection: Axis.horizontal,
+             children: [
+                _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+               _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+              _cardItem("Hotel 1", "assets/images/nemnig.jpg"),
+             ],
+               ),
+             ),
+            
+          ],
+         ),
+       ),
+     )
+     ],
       ),
     );
   }
@@ -138,8 +163,8 @@ class _HomePageState extends State<HomePage> {
 
 Widget _cardItem(String name, String img) {
   return Container(
-    margin: EdgeInsets.all(8),
-    width: 200,
+    margin: const EdgeInsets.all(8),
+    width: 180,
     decoration: BoxDecoration(
       borderRadius: BorderRadius.circular(20),
       image: DecorationImage(
@@ -159,10 +184,16 @@ Widget _cardItem(String name, String img) {
         ),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.only(left: 8),
         child: Align(
           alignment: Alignment.bottomLeft,
-          child: Text(name,style: TextStyle(color: Colors.white),)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(name,style: const TextStyle(color: Colors.white,fontSize: 18),),
+              IconButton(onPressed: (){}, icon: const Icon(Icons.favorite,color: Colors.red,))
+            ],
+          )),
       ),
     ),
   );
